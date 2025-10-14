e20feebe1692b833f415ed90e47b62cec1b0c457
Arijit <arijit123roy098@gmail.com>
2025-10-14 12:53:52 +0530

    ecom-first commit

---
+++ ecom/src/app/components/fooder/fooder.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fooder',
+  imports: [],
+  template: `
+    <p>
+      fooder works!
+    </p>
+  `,
+  styles: ``
+})
+export class Fooder {
+
+}
